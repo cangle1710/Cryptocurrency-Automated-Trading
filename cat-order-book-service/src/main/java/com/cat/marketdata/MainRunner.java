7588d5c52b12c7a64d6bc37b094f955fabbe02dc
Cang Le <cangle1710@gmail.com>
2020-12-28 13:30:22 -0600

    adding order book functionality

---
+++ cat-order-book-service/src/main/java/com/cat/marketdata/MainRunner.java
@@ -0,0 +1,27 @@
+package com.cat.marketdata;
+import java.util.logging.Logger;
+
+public class MainRunner {
+    private static final Logger logger = Logger.getLogger(MainRunner.class.getName());
+
+    public static void main(String[] args) {
+        String instrument = "";
+        if(args.length != 1){
+            logger.warning("Invalid input. Expecting 1 instrument to subscribe to for market data");
+            System.exit(0);
+        }
+        else{
+            instrument = args[0];
+        }
+
+        OrderBookService orderBookService = new OrderBookService(instrument);
+        // adding shut down hook to catch "Ctrl + C" shutdown command
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            logger.info("Shutting down ...");
+            orderBookService.doDisable();
+        }));
+
+        // starting order book service
+        orderBookService.doEnable();
+    }
+}
---
+++ cat-runner/src/main/java/com/cat/runner/MainRunner.java
@@ -0,0 +1,7 @@
+package com.cat.runner;
+
+public class MainRunner {
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+    }
+}
